aa9781fcc937648bd3bc3e7efa757df8334ca2ab
abdurrahmankaya71 <abdurrahmanky71@gmail.com>
2023-07-18 13:11:38 +0300

    Upload The Design

---
+++ main.js
@@ -0,0 +1,10 @@
+let fixImg = document.querySelector(".fix-img");
+let container = document.querySelector(".container");
+
+function phones(x) {
+  fixImg.src = x;
+}
+
+function colors(color) {
+  container.style.background = color;
+}
